82c7f7bfecc0c7c6b1f9f2c9a0c6d6c052f0d14b
annttonnio <antonio56.lop@gmail.com>
2021-11-18 13:14:55 +0100

    01 motor

---
+++ Motor.java
@@ -0,0 +1,16 @@
+public class Motor
+{
+    private boolean encendido;
+    private String potenciaMotor;
+
+    public Motor(boolean encender){
+        encendido = encender;
+        potenciaMotor = "6 Kw";
+    }
+
+
+    /*public int sampleMethod(int y)
+    {
+        return x + y;
+    }*/
+}
